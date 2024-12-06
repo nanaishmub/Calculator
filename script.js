de6fe0c24cfa2cc1291a1d6917a6ff5ae6052921
nanaishmub <aisha.mujahid.kabir@gmail.com>
2024-12-07 00:18:34 +0100

    added display functions

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 // your logic here
+document.addEventListener("DOMContentLoaded", () => {
+    /*const table = document.getElementById("calcu")*/
+   
+
+    function displayValues() {
+        const buttons = document.querySelectorAll("#calcu input[type='button']");
+        const result = document.getElementById("result");
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                const value = button.value;
+
+                if (value ==="c"){
+                    result.value = "";
+                } else{
+                    result.value += value;
+                }
+            })
+        })
+    }
+
+    function performOperations() {
+        const buttons = document.getElementsByClassName("operations");
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                const value = button.value;
+        
+                    if (value === "+"){
+                        result.value = value + value;
+                    } else if(value === "-"){
+                        result.value = value - value;
+                    } else if(value === "*"){
+                      result.value = value * value;
+                    } else if(value === "/"){
+                        if ()
+                        result.value = value / value;
+                    } else if(value === "-"){
+                        result.value = value - value;
+                    }
+                }
+            })
+        })
+
+    }
+
+
+    displayValues();
+    //performOperations();
+})
